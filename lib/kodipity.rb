e81ed550f48a34023245295005b70b4a87c96d91
kirbycm <kirbycm@gmail.com>
2016-11-28 20:46:06 -0500

    Working recording details.

--- lib/kodipity.rb
+++ lib/kodipity.rb
@@ -9,21 +9,40 @@ module Kodipity
 	@movies = '{"jsonrpc": "2.0", "method": "VideoLibrary.GetMovies", "params": { "properties" : ["art", "rating", "thumbnail", "playcount", "file"], "sort": { "order": "ascending", "method": "label", "ignorearticle": true } }, "id": "libMovies"}'
 	@player_active = '{"jsonrpc": "2.0", "id": 1, "method": "Player.GetProperties", "params": {"properties": ["speed"], "playerid": 1}}'
 	@channels = '{"jsonrpc": "2.0", "id": 1, "method": "PVR.GetChannelGroups", "params": {"channeltype" : "tv"}, "playerid": 1}}'
+	@recordings = '{"jsonrpc": "2.0", "id": 1, "method": "PVR.GetRecordings", "playerid": 1}}'
 
 	def self.movies
-		HTTParty.post @url, headers: @headers, body: @movies
+		movies = {}
+		HTTParty.post(@url, headers: @headers, body: @movies)['result']['movies'].each do |movie|
+			movies[movie['label']] = movie['movieid']
+		end
+		movies
 	end
 
 	def self.channels
 		channel_groups_resp = HTTParty.post @url, headers: @headers, body: @channels
 		channel_groups = channel_groups_resp['result']['channelgroups'].map{ |group| group['channelgroupid'] }
-		channels = []
+		channels = {}
 
 		channel_groups.each do |id|
-			@channel_group = {jsonrpc: '2.0', id: 1, method: 'PVR.GetChannels', params: {channelgroupid: id}, playerid: 1}
-			channels << HTTParty.post(@url, headers: @headers, body: @channel_group.to_json)
+			channel_group = {jsonrpc: '2.0', id: 1, method: 'PVR.GetChannels', params: {channelgroupid: id}, playerid: 1}
+			HTTParty.post(@url, headers: @headers, body: channel_group.to_json)['result']['channels'].each do |channel|
+				channels[channel['label']] = channel['channelid']
+			end
 		end
 		channels
+	end
+
+	def self.recordings
+		HTTParty.post(@url, headers: @headers, body: @recordings)
+	end
+
+	def self.rec
+		HTTParty.post(@url, headers: @headers, body: '{"jsonrpc": "2.0", "id": 1, "method": "PVR.GetRecordingDetails", "params": {"recordingid" : 362, "properties": ["title"]}, "playerid": 1}')
+	end
+
+	def self.doc(param)
+		HTTParty.post(@url, headers: @headers, body: '{ "jsonrpc": "2.0", "method": "JSONRPC.Introspect", "params": { "filter": { "id": "PVR.GetRecordingDetails", "type": "method" } }, "id": 1 }')
 	end
 
 end
